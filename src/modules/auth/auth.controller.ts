d103c670882ddce649b1ba96a8536531702a05a7
maria-kashpur <kashpur.maria@icloud.com>
2024-03-12 08:52:03 +0300

    feat: add auth guard

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -8,22 +8,26 @@ import { AuthService } from './auth.service';
 import { SignupAuthDto } from './dto/signup-auth.dto';
 import { RefreshAuthDto } from './dto/refresh-auth.dto';
 import { LoginAuthDto } from './dto/login-auth.dto';
+import { Public } from './decorators/public.decorators';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('signup')
+  @Public()
   async signup(@Body() dto: SignupAuthDto) {
     return await this.authService.signup(dto);
   }
 
   @Post('login')
+  @Public()
   async login(@Body() dto: LoginAuthDto) {
     return await this.authService.login(dto);
   }
 
   @Post('refresh')
+  @Public()
   @HttpCode(200)
   async refresh(@Body() dto: RefreshAuthDto) {
     return await this.authService.refresh(dto);
--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,11 +2,17 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserModule } from '../user/user.module';
-import { JwtService } from '@nestjs/jwt';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './auth.guard';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, JwtModule.register({ global: true })],
   controllers: [AuthController],
-  providers: [AuthService, JwtService],
+  providers: [
+    AuthService,
+    JwtService,
+    { provide: APP_GUARD, useClass: AuthGuard },
+  ],
 })
 export class AuthModule {}
